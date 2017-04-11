5fd1ba24573c6d81582a382f0d68a5f8dde5bee4
Nicholas Pushkin <nicholas.pushkin@gmail.com>
2017-04-11 17:35:53 +0300

    fix: Fix print py27 style to py36. Add dataset converting tool. Now working

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python
 
+
+original = "../final_project/final_project_dataset.pkl"
+destination = "../final_project/final_project_dataset_unix.pkl"
+
+content = ''
+outsize = 0
+with open(original, 'rb') as infile:
+    content = infile.read()
+    
+with open(destination, 'wb') as output:
+    for line in content.splitlines():
+        outsize += len(line) + 1
+        output.write(line + str.encode('\n'))
+
+print("Done. Saved %s bytes." % (len(content)-outsize))
+
+
+
+
 import pickle
 import sys
 import matplotlib.pyplot
@@ -8,12 +27,18 @@ from feature_format import featureFormat, targetFeatureSplit
 
 
 ### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load( open("../final_project/final_project_dataset_unix.pkl", "rb") )
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
-
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
