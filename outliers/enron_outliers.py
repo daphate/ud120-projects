a97934f1cb2022d09fd4baaedbec92ecde70d1ed
Nicholas Pushkin <nicholas.pushkin@gmail.com>
2017-04-11 17:42:18 +0300

    feat: Add labels to our features

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -1,24 +1,5 @@
 #!/usr/bin/python
 
-
-original = "../final_project/final_project_dataset.pkl"
-destination = "../final_project/final_project_dataset_unix.pkl"
-
-content = ''
-outsize = 0
-with open(original, 'rb') as infile:
-    content = infile.read()
-    
-with open(destination, 'wb') as output:
-    for line in content.splitlines():
-        outsize += len(line) + 1
-        output.write(line + str.encode('\n'))
-
-print("Done. Saved %s bytes." % (len(content)-outsize))
-
-
-
-
 import pickle
 import sys
 import matplotlib.pyplot
@@ -28,13 +9,16 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset_unix.pkl", "rb") )
-features = ["salary", "bonus"]
+features = ["poi", "salary", "bonus"]
 data = featureFormat(data_dict, features)
 
+label, features = targetFeatureSplit(data)
+
+print(data_dict)
 
 ### your code below
 
-for point in data:
+for point in features:
     salary = point[0]
     bonus = point[1]
     matplotlib.pyplot.scatter( salary, bonus )
