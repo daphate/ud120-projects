0f6d7ab10428d45082fd98a2aae3e4147afafbd1
Nicholas Pushkin <nicholas.pushkin@gmail.com>
2017-04-16 09:03:37 +0300

    test: Correct

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -2,91 +2,88 @@
 
 import pickle
 import sys
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
+import numpy as np
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 from sklearn import linear_model
-from sklearn.cross_validation import train_test_split
-import numpy
-import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
-### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset_unix.pkl", "rb") )
+### read in data dictionary, convert to np array
+data_dict = pickle.load( open("../final_project/final_project_dataset_unix_1.pkl", "rb") )
 
 ### your code below
 
-labels = data_dict.keys()
+for key in data_dict.keys():
+    if data_dict[key]["salary"] == "NaN":
+        data_dict[key]["salary"] = 0
+    if data_dict[key]["bonus"] == "NaN":
+        data_dict[key]["bonus"] = 0
+    if int(data_dict[key]["salary"]) > 1000000 and int(data_dict[key]["bonus"]) > 5000000:
+        print(key) 
+
 features = ["poi", "salary", "bonus"]
 data = featureFormat(data_dict, features)
 poi, features = targetFeatureSplit(data)
 
-salaries = []
-bonuses = []
+features = np.reshape( np.array(features), (len(features), 2))
+salaries, bonuses = np.split(features, 2, 1)
+#features = np.array(features)
 
-for i, item in enumerate(features):
-    salaries.append(item[0])
-    bonuses.append(item[1])
-
-#print(label, salaries, bonuses)
-
-salaries = numpy.reshape( numpy.array(salaries), (len(salaries), 1))
-bonuses = numpy.reshape( numpy.array(bonuses), (len(bonuses), 1))
-
-from sklearn.cross_validation import train_test_split
 salaries_train, salaries_test, bonuses_train, bonuses_test = train_test_split(salaries, bonuses, test_size=0.1, random_state=42)
 
 reg = linear_model.LinearRegression()
-
-# Train the model using the training sets
-reg.fit(salaries_train, bonuses_train)
-pred = reg.predict(salaries_test)
+reg.fit(salaries, bonuses)
+pred = reg.predict(salaries)
 
 # The coefficients
 print("Coefficients: ", reg.coef_, reg.intercept_)
 # The mean squared error
-print("Mean squared error: %.2f"
-     % numpy.mean(( pred - bonuses_test) ** 2))
-print('Variance score: %.2f' % reg.score(salaries_test, salaries_test))
+#print("Mean squared error: %.2f" % np.mean(( pred - bonuses_test) ** 2))
+#print('Variance score: %.2f' % reg.score(salaries_test, salaries_test))
 
 try:
     plt.plot(salaries, reg.predict(salaries))
 except NameError:
     pass
 plt.scatter(salaries, bonuses)
-plt.show()
-
-cleaned_data = []
-
-### your code goes here
-
-for i, item in enumerate(bonuses):
-    error = pred[i] - item
-    t = (int(salaries[i]), float(bonuses[i]), numpy.abs(float(error)))
-    cleaned_data.append(t)
-
-cleaned_data = sorted(cleaned_data, key = lambda cleaned_data: cleaned_data[2])
-cleaned_data = cleaned_data[:int(round(len(cleaned_data)*.9))]
-
-salaries, bonuses, errors = zip(*cleaned_data)
-salaries = numpy.reshape( numpy.array(salaries), (len(salaries), 1))
-bonuses = numpy.reshape( numpy.array(bonuses), (len(bonuses), 1))
-
-try:
-    reg.fit(salaries, bonuses)
-    plt.plot(salaries, reg.predict(salaries))
-except NameError:
-    print ("Somewhat error in cleaning")
-
-plt.scatter(ages, net_worths)
 plt.xlabel("salaries")
 plt.ylabel("bonuses")
 plt.show()
 
-for point in features:
-    salary = point[0]
-    bonus = point[1]
-    plt.scatter( salary, bonus )
+cleaned_data = []
 
-plt.xlabel("salary")
-plt.ylabel("bonus")
+for i, item in enumerate(bonuses):
+    error = pred[i] - item
+    t = (int(salaries[i]), int(bonuses[i]), np.abs(float(error)))
+    cleaned_data.append(t)
+
+print (cleaned_data, len(cleaned_data))
+
+cleaned_data = sorted(cleaned_data, key = lambda cleaned_data: cleaned_data[2])
+cleaned_data = cleaned_data[:int(94)]
+print (cleaned_data, len(cleaned_data))
+
+salaries, bonuses, errors = zip(*cleaned_data)
+salaries = np.reshape( np.array(salaries), (len(salaries), 1))
+bonuses = np.reshape( np.array(bonuses), (len(bonuses), 1))
+
+reg.fit(salaries, bonuses)
+pred = reg.predict(salaries_test)
+
+# The coefficients
+print("Coefficients: ", reg.coef_, reg.intercept_)
+# The mean squared error
+#print("Mean squared error: %.2f" % np.mean(( pred - bonuses_test) ** 2))
+#print('Variance score: %.2f' % reg.score(salaries_test, salaries_test))
+
+try:
+    plt.plot(salaries, reg.predict(salaries))
+except NameError:
+    print ("Somewhat error in cleaning")
+
+plt.scatter(salaries, bonuses)
+plt.xlabel("salaries")
+plt.ylabel("bonuses")
 plt.show()
