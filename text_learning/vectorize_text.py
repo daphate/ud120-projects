4b6804f1c985537a09939e25ebb2ca8dae3e90c3
Nicholas Pushkin <nicholas.pushkin@gmail.com>
2017-04-16 08:36:01 +0300

    feat: Finished words bag collecting and Tfidf vectorizing.

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -7,6 +7,8 @@ import sys
 
 sys.path.append( "../tools/" )
 from parse_out_email_text import parseOutText
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk import corpus
 
 """
     Starter code to process the emails from Sara and Chris to extract
@@ -22,7 +24,7 @@ from parse_out_email_text import parseOutText
     The data is stored in lists and packed away in pickle files at the end.
 """
 
-
+"""
 from_sara  = open("from_sara.txt", "r")
 from_chris = open("from_chris.txt", "r")
 
@@ -36,40 +38,58 @@ word_data = []
 ### can iterate your modifications quicker
 temp_counter = 0
 
-
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
+
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
         temp_counter += 1
-        if temp_counter < 200:
+       
+        if temp_counter >= 0:
+
+            if name == "sara":
+                from_data.append([0])
+            if name == "chris":
+                from_data.append([1])
+                        
             path = os.path.join('..', path[:-1])
-            print path
+            print(path)
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-
             ### append the text to word_data
-
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
 
-
+            tmp = parseOutText(email)
             email.close()
+            
+            tmp = tmp.replace("sara", "")
+            tmp = tmp.replace("chris", "")
+            tmp = tmp.replace("shackleton", "")
+            tmp = tmp.replace("germani", "")
+                   
+            word_data.append(tmp)
+            print(tmp)
 
-print "emails processed"
+print ("Emails processed", from_data)
+
 from_sara.close()
 from_chris.close()
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+pickle.dump( word_data, open("your_word_data.pkl", "wb") )
+pickle.dump( from_data, open("your_email_authors.pkl", "wb") )
 
-
-
-
+"""
 
 ### in Part 4, do TfIdf vectorization here
 
+word_data = pickle.load(open("your_word_data.pkl", "rb"))
+from_data = pickle.load(open("your_email_authors.pkl", "rb"))
 
+vect = TfidfVectorizer(stop_words='english')
+vect.fit(word_data)
+print(len(vect.get_feature_names()))
+
+print(vect.get_feature_names()[34597])
